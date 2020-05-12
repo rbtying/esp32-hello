4437418126042d636bdb9815433c41be39784d11
Robert Ying <rbtying@aeturnalus.com>
2020-05-11 17:11:36 -0700

    Use docker container for build tools

--- main/bindgen.sh
+++ main/bindgen.sh
@@ -2,16 +2,14 @@
 
 set -eu
 
-INCLUDES=`cat ../build/esp-idf/main/main_includes.txt`
-COMPS=$IDF_PATH/components
-: "${SYSROOT:=$($XTENSA_LLVM_ROOT/bin/clang --print-resource-dir)}"
+INCLUDES=$(cat ../build/esp-idf/main/main_includes.txt | python -c "import os.path, sys; print(' '.join(['-I' + os.path.relpath(s.strip()[1:-1], '..') for s in sys.stdin.readlines()]))")
+
+: "${SYSROOT:=$(docker run --rm rbtying/esp-crossbuild-env /opt/llvm-xtensa/bin/clang --print-resource-dir)}"
 TARGET=xtensa-esp32-none-elf
-: "${BINDGEN:=bindgen}"
-: "${LIBCLANG_PATH:=$XTENSA_LLVM_ROOT/lib/}"
 CLANG_FLAGS="\
     --sysroot=$SYSROOT \
     $INCLUDES \
-    -I"$COMPS/newlib/include" \
+    -I"/esp-idf/components/newlib/include" \
     -I"$(pwd)" \
     -D__bindgen \
     --target=$TARGET \
@@ -21,17 +19,16 @@ CLANG_FLAGS="\
 generate_bindings()
 {
     # --no-rustfmt-bindings because we run rustfmt separately with regular rust
-    LIBCLANG_PATH="$LIBCLANG_PATH" \
-    "$BINDGEN" \
+    docker run --rm --mount type=bind,source=$IDF_PATH,target=/esp-idf --mount type=bind,source=$(pwd)/..,target=/project rbtying/esp-crossbuild-env bindgen \
         --use-core \
         --ctypes-prefix crate::types \
         --no-layout-tests \
         --no-rustfmt-bindings \
-        --output esp-idf-sys/src/bindings.rs \
-        esp-idf-sys/src/bindings.h \
+        --output main/esp-idf-sys/src/bindings.rs \
+        main/esp-idf-sys/src/bindings.h \
         -- $CLANG_FLAGS
 
-    rustup run stable rustfmt esp-idf-sys/src/bindings.rs
+    docker run --rm --mount type=bind,source=$(pwd),target=/project rbtying/esp-crossbuild-env rustup run stable rustfmt esp-idf-sys/src/bindings.rs
 }
 
 generate_bindings "$@"
